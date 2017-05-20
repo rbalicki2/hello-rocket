39eed9e38d3bb9f6306d4dd0a37bb177f1b418e5
Robert <robert.balicki@gmail.com>
2017-05-20 17:58:37 -0400

    clean up styles a bit

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -10,37 +10,33 @@ pub mod db;
 pub mod schema;
 pub mod errors;
 
-use models::GetUser;
-use models::PostUser;
+use models::User;
+use models::NewUser;
 use models::Id;
 
 use self::errors::{Result, Error, ErrorKind, ResultExt};
 
 #[get("/users/<id>")]
-pub fn get_user(db_pool: State<db::ConnectionPool>, id: Id) -> Result<JSON<GetUser>> {
+pub fn get_user(db_pool: State<db::ConnectionPool>, id: Id) -> Result<JSON<User>> {
   use self::schema::users::dsl::users;
   let conn: db::DbConnection = db_pool.get().chain_err(|| "Could not connect to DB")?;
 
-  let user: GetUser = users
+  let user: User = users
     .find(id)
-    .first::<GetUser>(&*conn)
+    .first::<User>(&*conn)
     .chain_err(|| "Could not find user")?;
 
   Ok(JSON(user))
 }
 
 #[post("/users", data="<user_data>")]
-pub fn create_user(db_pool: State<db::ConnectionPool>, user_data: JSON<PostUser>) -> Result<JSON<GetUser>> {
-//  use self::schema::users::dsl::users;
-//  let conn: db::DbConnection = db_pool.get().chain_err(|| "Could not connect to DB")?;
-  // TODO use the pooled connection. But:
-  // the trait `diesel::Connection` is not implemented for
-//   `r2d2::PooledConnection<r2d2_diesel::ConnectionManager<diesel::pg::PgConnection>>`
+pub fn create_user(user_data: JSON<NewUser>) -> Result<JSON<User>> {
+  // TODO figure out why you can't use a connection pool
   let conn = db::establish_connection();
 
-  let user: PostUser = user_data.0;
+  let user: NewUser = user_data.0;
 
-  let returned_user: GetUser = diesel::insert(&user)
+  let returned_user: User = diesel::insert(&user)
     .into(schema::users::table)
     .get_result(&conn)
     .chain_err(|| "Error inserting user")?;
--- src/models/user.rs
+++ src/models/user.rs
@@ -2,13 +2,13 @@ use models::Id;
 use app::schema::*;
 
 #[derive(Queryable, Serialize, Deserialize, Clone)]
-pub struct GetUser {
+pub struct User {
   pub id: Id,
   pub name: String,
 }
 
 #[table_name="users"]
 #[derive(Insertable, Deserialize, Clone)]
-pub struct PostUser {
+pub struct NewUser {
   pub name: String,
 }
