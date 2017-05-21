cdace972008af7d1a04343c108f99835f296dae5
Robert <robert.balicki@gmail.com>
2017-05-21 03:00:04 -0400

    get limit and offset working

--- src/routes/user_routes.rs
+++ src/routes/user_routes.rs
@@ -7,7 +7,6 @@ use models::User;
 use models::NewUser;
 use models::Id;
 use models::LimitOffsetParam;
-use models::Combo;
 
 use app::errors::{Result, ResultExt};
 use app::db;
@@ -50,17 +49,17 @@ add_field_names! {
   }
 }
 
-#[get("/users?<query_params>")]
-pub fn get_users(db_pool: State<db::ConnectionPool>, query_params: Combo<LimitOffsetParam, MyStruct>) -> Result<JSON<Vec<User>>> {
+#[get("/users?<limit_offset>")]
+pub fn get_users(db_pool: State<db::ConnectionPool>, limit_offset: LimitOffsetParam) -> Result<JSON<Vec<User>>> {
   use app::schema::users::dsl::users;
   let conn: db::DbConnection = db_pool.get().chain_err(|| "Could not connect to DB")?;
 
-  let limit: i64 = query_params.first.limit.unwrap_or(100) as i64;
-//  let offset: i64 = limit_offset.limit.unwrap_or(0) as i64;
+  let limit: i64 = limit_offset.limit.unwrap_or(100) as i64;
+  let offset: i64 = limit_offset.offset.unwrap_or(0) as i64;
 
   let users_response: Vec<User> = users
+    .offset(offset)
     .limit(limit)
-//    .offset(offset)
     .load::<User>(&*conn)
     .chain_err(|| "Could not query users")?;
 
