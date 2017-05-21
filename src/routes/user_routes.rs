7ad110212b13de43d20b349ff377e9c1f1aae33a
Robert <robert.balicki@gmail.com>
2017-05-21 16:51:45 -0400

    it works

--- src/routes/user_routes.rs
+++ src/routes/user_routes.rs
@@ -2,7 +2,7 @@ use diesel;
 use rocket_contrib::JSON;
 use rocket::State;
 use diesel::prelude::*;
-//use itertools::Itertools;
+use itertools::Itertools;
 
 use models::User;
 use models::NewUser;
@@ -34,23 +34,24 @@ pub fn create_user(db_pool: State<db::ConnectionPool>, user_data: JSON<NewUser>)
 }
 
 #[get("/users?<limit_offset>")]
-pub fn get_users(db_pool: State<db::ConnectionPool>, limit_offset: LimitOffsetParam) -> Result<JSON<Vec<i32>>> {
+pub fn get_users(db_pool: State<db::ConnectionPool>, limit_offset: LimitOffsetParam) -> Result<JSON<Vec<User>>> {
   use app::schema::users::dsl::users;
   let conn: db::DbConnection = db_pool.get().chain_err(|| "Could not connect to DB")?;
 
   let limit: i64 = limit_offset.limit.unwrap_or(100) as i64;
   let offset: i64 = limit_offset.offset.unwrap_or(0) as i64;
 
-  let users_response: Vec<i32> = users
+  let users_response: Vec<User> = users
     .left_outer_join(schema::photos::table)
     .load(&*conn)
     .chain_err(|| "could not load")?
     .iter()
-    .map(|x: (User, Photo)| 42)
+    .map(|x: &(User, Option<Photo>)| x.0.clone())
+//    .iter()
+//    .map(|x: &(User, Vec<Photo>)| x.0.clone())
     .collect();
 //    .iter()
 
-//    .group_by(|(user, photo)| user)
 //    .map(|(user, photos)| user)
 //    .collect();
 //    .offset(offset)
